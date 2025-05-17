f145d644231b0adf38ee0f013cf4307b106f7188
DavinTanaya <davin.tanaya@gmail.com>
2025-05-18 02:24:52 +0700

    feat: allow cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // kamu bisa atur semua route API, atau hanya route tertentu
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin",      value: "*"        },
+          { key: "Access-Control-Allow-Methods",     value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers",     value: "*"        },
+          { key: "Access-Control-Allow-Credentials", value: "true"     },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
